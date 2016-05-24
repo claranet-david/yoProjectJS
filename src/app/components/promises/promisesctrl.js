db785800405e6e56fd0ad1d69e3ff44ff15540cf
David Rivera Muñoz (thabeat) <thabeat@gmail.com>
2016-05-24 17:21:09 +0200

    Added Promises Test Page

--- src/app/components/promises/promisesctrl.js
+++ src/app/components/promises/promisesctrl.js
@@ -3,13 +3,52 @@
 		.module('yoProjectJS')
 		.controller('PromisesController', PromisesController);
 
-		PromisesController.$inject = ['$scope', '$http', 'loginService', 'customAWSService'];
+		PromisesController.$inject = ['$scope', '$http', '$q', '$timeout', 'loginService', 'customAWSService'];
 
-		function PromisesController($scope, $http, loginService, customAWSService){
+		function PromisesController($scope, $http, $q, $timeout, loginService, customAWSService){
 			
 			var vm = this;
+			vm.response=vm.iterations=0;
+			vm.ongoing = "WORKING";
 
-			vm.response="HELLO!";
+			function add(x,y){
+				return $timeout( function(){
+					//console.log(x+y);
+					//callback(x+y);
+					vm.iterations = x+y;
+					return (x+y);
+				}, 2000);
+			}
+
+			var startTime = Date.now();
+			add(0 ,1)
+				.then(function(response){
+					return add(response, 1);
+				})
+				.then(function(response){
+					return add(response, 1);
+				})
+				.then(function(response){
+					return add(response, 1);
+				})
+				.then(function(response){
+					return add(response, 1);
+				})
+				.then(function(response){
+					vm.response = response;
+					vm.ongoing = "DONE!";
+				})
+				.finally(function(){
+					vm.elapsedTime = Date.now() - startTime;
+					//console.log(vm.elapsedTime);
+				});
+
+
+
+				
+
+			
+
 
 		}
 })();
